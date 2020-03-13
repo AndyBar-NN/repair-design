61cc2fb406d171b58f5374fc5eec8e08ab1dc389
Andrey1221 <andreysparrow91@rambler.ru>
2020-03-13 16:35:55 +0300

    add arrow scroll up button

--- js/main.js
+++ js/main.js
@@ -24,3 +24,18 @@ $(document).ready(function (){
     modal.toggleClass('modal--visible');
   });
 });
+
+$(function(){
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 100) {
+      $('#scroll_top').show();
+    } else {
+      $('#scroll_top').hide();
+    }
+  });
+
+  $('#scroll_top').click(function(){
+    $('html, body').animate({scrollTop: 0}, 600);
+    return false;
+  });
+});
