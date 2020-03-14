23175a72ccc71050600792f2976b0f02a569c4b0
Andrey1221 <andreysparrow91@rambler.ru>
2020-03-14 16:31:44 +0300

    add swiper slide

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-
-  closeBtn.addEventListener('click', switchModal);
-});*/
-
 $(document).ready(function (){
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
@@ -38,4 +23,23 @@ $(function(){
     $('html, body').animate({scrollTop: 0}, 600);
     return false;
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 20 + bullets.width() + 20)
+  bullets.css('left', prev.width() + 20)
 });
